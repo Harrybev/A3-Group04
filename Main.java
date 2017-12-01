90e50991083fd0e3120089dadf746efdab17e893
Harrybev <33958624+Harrybev@users.noreply.github.com>
2017-12-01 17:39:34 +0000

    Comments and Input error cleanup
    didn't like the rigidness of my code (the way it rejected a lot of inputs) so now its a lot more flexible with what it can take also i've added the necessary comments where they were needed on my classes

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		WriteFiles runTest = new WriteFiles();
+		ReadFiles runTest2 = new ReadFiles();
+		runTest.writeUsers(runTest2.readUsers());
+		runTest.writeArtworks(runTest2.readArtworks());
+		runTest.writeAuctions(runTest2.readAuctions(runTest2.readUsers(), runTest2.readArtworks()));
+	}
+}
