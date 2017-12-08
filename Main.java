bb16e46cfaa3bdc83f7d3f2fe9af5435c9a519b0
Tulsi Patel <tpatel219@outlook.com>
2017-12-08 16:58:03 +0000

    Wrong Place got deleted

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-	public static void main(String[] args) {
-		WriteFiles runTest = new WriteFiles();
-		ReadFiles runTest2 = new ReadFiles();
-		runTest.writeUsers(runTest2.readUsers());
-		runTest.writeArtworks(runTest2.readArtworks());
-		runTest.writeAuctions(runTest2.readAuctions(runTest2.readUsers(), runTest2.readArtworks()));
-	}
-}
