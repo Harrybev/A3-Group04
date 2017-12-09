40da0d01428b7108c3d24c3c41f8fe3311f3942e
Christopher Evans <cevans88@hotmail.co.uk>
2017-12-09 00:52:29 +0000

    Got ProfileSettings GUI to display a ProfileImage.

--- src/ProfileSettingsController.java
+++ src/ProfileSettingsController.java
@@ -1,12 +1,20 @@
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+
 
 public class ProfileSettingsController {
 
     @FXML Button btnChangeProfilePicture;
+    @FXML ImageView ivProfilePic;
 
     public void initialize() {
 
+        Image profileImage = new Image("profile-pics/1.jpg");
+        ivProfilePic.setImage(profileImage);
 
         btnChangeProfilePicture.setOnAction(event -> {
             handleBtnChangeProfilePicture();
