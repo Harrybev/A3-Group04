16404b2c6c4944270dabad10b11c867fa779c91f
Christopher Evans <cevans88@hotmail.co.uk>
2017-12-10 22:50:02 +0000

    Fixed MyAuctions logic.

--- src/SingleAuctionViewController.java
+++ src/SingleAuctionViewController.java
@@ -1,11 +1,17 @@
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 
 public class SingleAuctionViewController {
 
     @FXML
     Label lblArtworkDetails;
+    Pane paneDynamicBidOrHistory;
+
+
+    private Auction selectedAuction;
     public void initialize() {
+
 
     }
 }
